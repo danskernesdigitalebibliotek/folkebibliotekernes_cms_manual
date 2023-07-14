f65ef25f8d9cbf86d61c42dfc03c33ee578e893e
agnetemoos <agnetemoos@users.noreply.github.com>
2023-07-14 07:27:59 +0000

    Create/update Jekyll site for agnetemoos-change 6bfe7cd4cd58a7fa7669b37021fa0d88fc39c34b

---
+++ preview/agnetemoos-change/assets/js/lunr/lunr-store.js
@@ -0,0 +1,43 @@
+var store = [{
+        "title": "Administrator",
+        "excerpt":"Administrator  ","categories": [],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-change/audience/administrator/",
+        "teaser": null
+      },{
+        "title": "Redaktør",
+        "excerpt":"Redaktør  ","categories": [],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-change/audience/redaktoer/",
+        "teaser": null
+      },{
+        "title": "Introduktion",
+        "excerpt":"Forside, læsevejledning  ","categories": [],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-change/category/introduktion/",
+        "teaser": null
+      },{
+        "title": "CMS Manual",
+        "excerpt":"I løbet af efteråret udbygges manual til Folkebibliotekernes CMS på disse sider. Vi har valgt en åben proces, hvor bibliotekerne kan se manual-siderne, efterhånden som de bliver skrevet.   Her er en ændring fra et pull request.  ","categories": ["Introduktion"],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-change/manual/",
+        "teaser": null
+      },{
+        "title": "Betaling",
+        "excerpt":"Betaling  ","categories": [],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-change/topic/betaling/",
+        "teaser": null
+      },{
+        "title": "Formidling",
+        "excerpt":"Formidling  ","categories": [],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-change/topic/formidling/",
+        "teaser": null
+      },{
+        "title": "Grundkonfiguration",
+        "excerpt":"Grundkonfiguration  ","categories": [],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-change/topic/grundkonfiguration/",
+        "teaser": null
+      }]
