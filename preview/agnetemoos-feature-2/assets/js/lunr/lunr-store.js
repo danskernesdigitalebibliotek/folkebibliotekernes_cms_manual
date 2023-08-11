db00ecf8435065b4d3d41e7cac84ea6a8b1478e0
agnetemoos <agnetemoos@users.noreply.github.com>
2023-08-11 12:33:04 +0000

    Create/update Jekyll site for agnetemoos-feature-2 21b1ea88bdc0f0ef576f3cba1fddd72c72f5cd9b

--- preview/agnetemoos-feature-2/assets/js/lunr/lunr-store.js
+++ preview/agnetemoos-feature-2/assets/js/lunr/lunr-store.js
@@ -11,28 +11,76 @@ var store = [{
         "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-feature-2/audience/redaktoer/",
         "teaser": null
       },{
-        "title": "Kategori 1",
-        "excerpt":"Kategori 1  ","categories": [],
+        "title": "Feature kategori",
+        "excerpt":"Feature kategori  ","categories": [],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-feature-2/category/feature-kategori/",
+        "teaser": null
+      },{
+        "title": "AKategori 1",
+        "excerpt":"AKategori 1  ","categories": [],
         "tags": [],
         "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-feature-2/category/kategori1/",
         "teaser": null
       },{
-        "title": "Kategori 2",
-        "excerpt":"Kategori 2  ","categories": [],
+        "title": "Categori 2",
+        "excerpt":"Categori 2  ","categories": [],
         "tags": [],
         "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-feature-2/category/kategori2/",
         "teaser": null
       },{
         "title": "CMS Manual",
-        "excerpt":"I løbet af efteråret udbygges manual til Folkebibliotekernes CMS på disse sider. Vi har valgt en åben proces, hvor bibliotekerne kan se manual-siderne, efterhånden som de bliver skrevet.  ","categories": ["Kategori 1"],
+        "excerpt":"I løbet af efteråret udbygges manual til Folkebibliotekernes CMS på disse sider. Vi har valgt en åben proces, hvor bibliotekerne kan se manual-siderne, efterhånden som de bliver skrevet.  ","categories": ["AKategori 1"],
         "tags": [],
         "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-feature-2/manual/",
         "teaser": null
       },{
-        "title": "Feature 1",
-        "excerpt":"Her er feature 1 Min url er: feature/feature1/  ","categories": ["Kategori 1"],
+        "title": "Manual side 1",
+        "excerpt":"Manual side 1  ","categories": ["AKategori 1"],
         "tags": [],
-        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-feature-2/nye_features/feature1/",
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-feature-2/manual/manpage1/",
+        "teaser": null
+      },{
+        "title": "Manual side 2",
+        "excerpt":"Manual side 2  ","categories": ["Categori 2"],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-feature-2/manual/manpage2/",
+        "teaser": null
+      },{
+        "title": "Feature 1",
+        "excerpt":"Her er feature 1 Min url er: feature/feature1/  ","categories": ["Feature kategori"],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-feature-2/nye-features/feature1/",
+        "teaser": null
+      },{
+        "title": "Feature 2",
+        "excerpt":"Her er feature 2  ","categories": ["Categori 2"],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-feature-2/nye-features/feature2/",
+        "teaser": null
+      },{
+        "title": "Nye features i Folkebibliotekernes CMS",
+        "excerpt":"   atomart design   gruppering af lån - de er “klappet sammen” omkring en dato   signalfarver   digitale lån   Forny ligger et andet sted   Skift afhentningssted ligger et andet sted   Sæt reserveringer på pause er flyttet  ","categories": ["Feature 1"],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-feature-2/nye-features/",
+        "teaser": null
+      },{
+        "title": "Aktiviteter i forbindelse med overgangen til Folkebibliotekernes CMS",
+        "excerpt":" ","categories": ["Feature 1"],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-feature-2/overgang-til-folkebibliotekernes-cms/",
+        "teaser": null
+      },{
+        "title": "Overgang1",
+        "excerpt":"Overgang1  ","categories": ["Feature kategori"],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-feature-2/overgang-til-folkebibliotekernes-cms/overgang1/",
+        "teaser": null
+      },{
+        "title": "Overgang 2",
+        "excerpt":"Her er overgang 2  ","categories": ["Categori 2"],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-feature-2/overgang-til-folkebibliotekernes-cms/overgang2/",
         "teaser": null
       },{
         "title": "Betaling",
