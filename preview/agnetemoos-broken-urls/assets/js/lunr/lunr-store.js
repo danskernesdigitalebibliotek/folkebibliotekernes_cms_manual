3fdd37f23bebcaa146c93f2563941716ad75439e
agnetemoos <agnetemoos@users.noreply.github.com>
2023-08-23 10:07:08 +0000

    Create/update Jekyll site for agnetemoos-broken-urls 4b335ec47f4958d591dbd454f96197e02b9b9cf4

---
+++ preview/agnetemoos-broken-urls/assets/js/lunr/lunr-store.js
@@ -0,0 +1,133 @@
+var store = [{
+        "title": "Opgave 4: Eksport af billeder eller lav nyt billedmateriale",
+        "excerpt":"Resultat Et billede siger mere end 1000 ord. Det ordsprog kender de fleste af os efterhånden, og der er noget om snakken. Billeder er visuelle udtryk, der kan give bedre forståelse for dit indhold på hjemmesiden, samtidig med at det gør teksten letfordøjelig. Med denne opgave skal I tage stilling...","categories": ["Overgangsaktiviter"],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-broken-urls/aktiviteter-ved-overgangen-til-folkebibliotekernes-cms/billedmateriale/",
+        "teaser": null
+      },{
+        "title": "Aktiviteter ved overgangen til Folkebibliotekernes CMS",
+        "excerpt":"Allerede nu kan I forberede jer på overgangen til Folkebibliotekernes CMS. Tag stilling til indholdet på jeres nuværende website og skitsér en plan for det nye. Kig på de nye designelementer og lad jer inspirere til en ny måde at strukturere og vise indhold. I seks punkter kommer I omkring...","categories": [],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-broken-urls/aktiviteter-ved-overgangen-til-folkebibliotekernes-cms/",
+        "teaser": null
+      },{
+        "title": "Opgave 3: Identitetsfarve",
+        "excerpt":"Beskrivelse Bibliotekets egen unikke identitetsfarve bruges på en lang række elementer i brugergrænsefladen. I har måske haft flere farver på jeres nuværende hjemmeside, men nu skal I kun vælge en. Metode og fremgangsmåde I skal vælge en identitetsfarve for jeres hjemmeside. I skal vælge en stærk og ren farve, da...","categories": ["Overgangsaktiviter"],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-broken-urls/aktiviteter-ved-overgangen-til-folkebibliotekernes-cms/identitetsfarve/",
+        "teaser": null
+      },{
+        "title": "Opgave 6: Informationsarkitektur og navigation",
+        "excerpt":"Beskrivelse Ligesom når man flytter ind i et nyt hus, så skal der også indrettes på en ny hjemmeside. Hvad vil I have hvor, og hvordan skal man kunne komme rundt mellem siderne? Der er ingen minimumskrav til hvor mange sider, artikler, licenser osv. der skal flyttes med over. Det...","categories": ["Overgangsaktiviter"],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-broken-urls/aktiviteter-ved-overgangen-til-folkebibliotekernes-cms/informationsarkiktektur/",
+        "teaser": null
+      },{
+        "title": "Opgave 5: Oprydning i artikler på det eksisterende site",
+        "excerpt":"Beskrivelse En flytning kan være en god lejlighed til at få ryddet op i gammelt indhold. I denne opgave giver jer nogle tips til at få kvalificeret det indhold, som skal med over på den nye hjemmeside. Metode og fremgangsmåde Hvad skal flyttes med over på den nye hjemmeside? Vi...","categories": ["Overgangsaktiviter"],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-broken-urls/aktiviteter-ved-overgangen-til-folkebibliotekernes-cms/oprydning/",
+        "teaser": null
+      },{
+        "title": "Opgave 1: Projektorganisering på biblioteket",
+        "excerpt":"Mål Målet med denne opgave er at få etableret en fornuftig organisering af arbejdet med at implementere Folkebibliotekernes CMS. Beskrivelse I skal nu til at opbygge en helt ny hjemmeside, og det kommer til at tage noget tid. Derfor er det vigtigt, at I får styr på, hvem der skal...","categories": ["Overgangsaktiviter"],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-broken-urls/aktiviteter-ved-overgangen-til-folkebibliotekernes-cms/projektorganisering-paa-biblioteket/",
+        "teaser": null
+      },{
+        "title": "Opgave 2: Tilretning af logo til nyt design ",
+        "excerpt":"Beskrivelse Det kan godt tage tid at designe et nyt logo, så tænk allerede nu over, om bibliotekets logo overholder de krav, der er til det nye CMS. Måske får det betydning for hele bibliotekets visuelle identitet, så det er vigtigt hurtigst muligt at få det på plads. Metode og...","categories": ["Overgangsaktiviter"],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-broken-urls/aktiviteter-ved-overgangen-til-folkebibliotekernes-cms/tilretning-af-logo-til-nyt-design/",
+        "teaser": null
+      },{
+        "title": "Administrator",
+        "excerpt":"Administrator  ","categories": [],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-broken-urls/audience/administrator/",
+        "teaser": null
+      },{
+        "title": "Redaktør",
+        "excerpt":"Redaktør  ","categories": [],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-broken-urls/audience/redaktoer/",
+        "teaser": null
+      },{
+        "title": "Feature kategori",
+        "excerpt":"Feature kategori  ","categories": [],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-broken-urls/category/feature-kategori/",
+        "teaser": null
+      },{
+        "title": "AKategori 1",
+        "excerpt":"AKategori 1  ","categories": [],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-broken-urls/category/kategori1/",
+        "teaser": null
+      },{
+        "title": "Categori 2",
+        "excerpt":"Categori 2  ","categories": [],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-broken-urls/category/kategori2/",
+        "teaser": null
+      },{
+        "title": "Overgangsaktiviter",
+        "excerpt":"Overgangsaktiviteter  ","categories": [],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-broken-urls/category/overgangsaktiviteter/",
+        "teaser": null
+      },{
+        "title": "CMS Manual",
+        "excerpt":"I løbet af efteråret udbygges manual til Folkebibliotekernes CMS på disse sider. Vi har valgt en åben proces, hvor bibliotekerne kan se manual-siderne, efterhånden som de bliver skrevet.  ","categories": [],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-broken-urls/manual/",
+        "teaser": null
+      },{
+        "title": "Manual side 1",
+        "excerpt":"Manual side 1  ","categories": ["AKategori 1"],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-broken-urls/manual/manpage1/",
+        "teaser": null
+      },{
+        "title": "Manual side 2",
+        "excerpt":"Manual side 2  ","categories": ["Categori 2"],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-broken-urls/manual/manpage2/",
+        "teaser": null
+      },{
+        "title": "Feature 1",
+        "excerpt":"Her er feature 1 Min url er: feature/feature1/  ","categories": ["Feature kategori"],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-broken-urls/nye-features/feature1/",
+        "teaser": null
+      },{
+        "title": "Feature 2",
+        "excerpt":"Her er feature 2  ","categories": ["Categori 2"],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-broken-urls/nye-features/feature2/",
+        "teaser": null
+      },{
+        "title": "Nye features i Folkebibliotekernes CMS",
+        "excerpt":"   atomart design   gruppering af lån - de er “klappet sammen” omkring en dato   signalfarver   digitale lån   Forny ligger et andet sted   Skift afhentningssted ligger et andet sted   Sæt reserveringer på pause er flyttet  ","categories": [],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-broken-urls/nye-features/",
+        "teaser": null
+      },{
+        "title": "Betaling",
+        "excerpt":"Betaling  ","categories": [],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-broken-urls/topic/betaling/",
+        "teaser": null
+      },{
+        "title": "Formidling",
+        "excerpt":"Formidling  ","categories": [],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-broken-urls/topic/formidling/",
+        "teaser": null
+      },{
+        "title": "Grundkonfiguration",
+        "excerpt":"Grundkonfiguration  ","categories": [],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/preview/agnetemoos-broken-urls/topic/grundkonfiguration/",
+        "teaser": null
+      }]
