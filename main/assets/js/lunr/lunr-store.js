9c1a8cc7ffcd9c572b793885564a992aed6fd27b
agnetemoos <agnetemoos@users.noreply.github.com>
2023-12-15 09:53:14 +0000

    Create/update Jekyll site for main 6ee915f0866c64237c4765b0402d004377e6fc5c

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -17,7 +17,7 @@ var store = [{
         "url": "/folkebibliotekernes_cms_manual/main/bliv-klar-til-folkebibliotekernes-cms/billedmateriale/",
         "teaser": null
       },{
-        "title": "Opgave 6: Forbered nyt indhold hvor det er påkrævet",
+        "title": "Opgave 6: Forbered nyt indhold, hvor det er påkrævet",
         "excerpt":"Beskrivelse Ikke alle komponenter og indholdstyper i det gamle CMS findes i det nye CMS. Nogle udvikles slet ikke og andre når ikke at blive klar inden indflytningsdatoen. Her er en oversigt over indhold, som I skal gentænke, hvis det skal overføres til Folkebibliotekernes CMS. Metode og fremgangsmåde Læs dette...","categories": ["Overgangsaktiviter"],
         "tags": [],
         "url": "/folkebibliotekernes_cms_manual/main/bliv-klar-til-folkebibliotekernes-cms/forbered-indhold/",
