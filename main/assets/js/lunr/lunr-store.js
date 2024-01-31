e775558c2be2b91d8e9d924a894a309757d928a9
agnetemoos <agnetemoos@users.noreply.github.com>
2024-01-31 08:39:26 +0000

    Create/update Jekyll site for main 613711d67efd2e59dbc308816955708ada55c681

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -312,7 +312,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Opgave 1: OpenID Connect (Adgangsplatformen)",
-        "excerpt":"Søgning i Folkebibliotekernes CMS sker via FBI-API. Læs mere om FBI-API. Adgang til FBI-API styres via Adgangsplatformen. Teknologien bag Adgangsplatformen hedder OpenID connect.   Kopier nøgler fra DDB CMS   Indsæt nøgler i Folkebibliotekernes CMS   Egenkontrol  Opgaven er løst korrekt  ","categories": ["Grundkonfiguration"],
+        "excerpt":"Adgangsplatformen er meget vigtig for Folkebibliotekernes CMS. Både søgning og brugerlogin forudsætter at bibliotekets adgangsnøgler til Adgangsplatformen er indsat korrekt i backend. Adgangsplatformen bygger på en teknologi der hedder OpenID Connect. Derfor omtales de ofte synonymt. I denne opgave skal i kopiere jeres OpenID Connect adgangsnøgler fra DDB CMS over...","categories": ["Grundkonfiguration"],
         "tags": [],
         "url": "/folkebibliotekernes_cms_manual/main/grundkonfiguration/openid-connect/",
         "teaser": null
