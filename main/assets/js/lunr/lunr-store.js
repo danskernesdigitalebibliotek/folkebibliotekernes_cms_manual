766ac784ce46be53cd41d7a022ba20461a3268f8
agnetemoos <agnetemoos@users.noreply.github.com>
2024-02-02 10:00:56 +0000

    Create/update Jekyll site for main b3cba108c6a8025bb119b5e930e7658bf7808f55

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -11,64 +11,70 @@ var store = [{
         "url": "/folkebibliotekernes_cms_manual/main/audience/redaktoer/",
         "teaser": null
       },{
-        "title": "Opgave 7: Eksport af billeder / nyt billedmateriale",
-        "excerpt":"Et billede siger mere end 1000 ord. Det ordsprog kender de fleste af os efterhånden, og der er noget om snakken. Billeder er visuelle udtryk, der kan give bedre forståelse for dit indhold på hjemmesiden, samtidig med at det gør teksten letfordøjelig. Med denne opgave skal I tage stilling til...","categories": ["Overgangsaktiviter"],
+        "title": "Opgave 10: Konfiguration af søge- og visningsprofiler",
+        "excerpt":"Hjemmesiden henter data fra databrønden i mange forskellige sammenhænge, og disse sammenhænge kan overordnet inddeles i søgning og visning.   Folkebibliotekernes CMS kræver to brøndprofiler i VIP-basen - en søgeprofil og en visningsprofil.  ","categories": ["Overgangsaktiviter"],
         "tags": [],
-        "url": "/folkebibliotekernes_cms_manual/main/bliv-klar-til-folkebibliotekernes-cms/billedmateriale/",
+        "url": "/folkebibliotekernes_cms_manual/main/bliv-klar-til-folkebibliotekernes-cms/10vip-profiler/",
+        "teaser": null
+      },{
+        "title": "Opgave1: Projektorganisering på biblioteket",
+        "excerpt":"Målet med denne opgave er at få etableret en fornuftig organisering af arbejdet med at implementere Folkebibliotekernes CMS. Beskrivelse I skal nu til at opbygge en helt ny hjemmeside, og det kommer til at tage noget tid. Derfor er det vigtigt, at I får styr på, hvem der skal gøre...","categories": ["Overgangsaktiviter"],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/main/bliv-klar-til-folkebibliotekernes-cms/1projektorganisering-paa-biblioteket/",
+        "teaser": null
+      },{
+        "title": "Opgave 2: Tilretning af logo til nyt design ",
+        "excerpt":"Det kan godt tage tid at designe et nyt logo, så tænk allerede nu over, om bibliotekets logo overholder de krav, der er til det nye CMS. Måske får det betydning for hele bibliotekets visuelle identitet, så det er vigtigt hurtigst muligt at få det på plads. Metode og fremgangsmåde...","categories": ["Overgangsaktiviter"],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/main/bliv-klar-til-folkebibliotekernes-cms/2logo/",
+        "teaser": null
+      },{
+        "title": "Opgave 3: Identitetsfarve",
+        "excerpt":"Bibliotekets egen unikke identitetsfarve bruges på en lang række elementer i brugergrænsefladen. I har måske haft flere farver på jeres nuværende hjemmeside, men nu skal I kun vælge en. Metode og fremgangsmåde I skal vælge en identitetsfarve for jeres hjemmeside. I skal vælge en stærk og ren farve, da den...","categories": ["Overgangsaktiviter"],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/main/bliv-klar-til-folkebibliotekernes-cms/3identitetsfarve/",
+        "teaser": null
+      },{
+        "title": "Opgave 4: Informationsarkitektur og navigation",
+        "excerpt":"Designbureauet Dept har lavet grunddesign til Folkebibliotekernes CMS. Dept har arbejdet med et særligt navigationsprincip, som det er værd at sætte sig ind i. Jeres nye hjemmeside bliver bedst, hvis I benytter design systemet, som det er tiltænkt. Med navigation hub konceptet navigerer man i dybden i stedet for bredden....","categories": ["Overgangsaktiviter"],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/main/bliv-klar-til-folkebibliotekernes-cms/4informationsarkiktektur/",
+        "teaser": null
+      },{
+        "title": "Opgave 5: Udvælg indhold der skal flyttes med over",
+        "excerpt":"En flytning kan være en god lejlighed til at få ryddet op i gammelt indhold. I denne opgave får I nogle tips til at få kvalificeret det indhold, som skal med over på den nye hjemmeside. Der er ingen minimumskrav til hvor mange sider, artikler, licenser osv. der skal flyttes...","categories": ["Overgangsaktiviter"],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/main/bliv-klar-til-folkebibliotekernes-cms/5oprydning/",
         "teaser": null
       },{
         "title": "Opgave 6: Forbered nyt indhold, hvor det er påkrævet",
         "excerpt":"Ikke alle komponenter og indholdstyper i det gamle CMS findes i det nye CMS. Nogle udvikles slet ikke og andre når ikke at blive klar inden indflytningsdatoen. Her er en oversigt over indhold, som I skal gentænke, hvis det skal overføres til Folkebibliotekernes CMS. Metode og fremgangsmåde Læs dette afsnit...","categories": ["Overgangsaktiviter"],
         "tags": [],
-        "url": "/folkebibliotekernes_cms_manual/main/bliv-klar-til-folkebibliotekernes-cms/forbered-indhold/",
+        "url": "/folkebibliotekernes_cms_manual/main/bliv-klar-til-folkebibliotekernes-cms/6forbered-indhold/",
+        "teaser": null
+      },{
+        "title": "Opgave 7: Billeder og filer/ nyt billedmateriale",
+        "excerpt":"Et billede siger mere end 1000 ord. Det ordsprog kender de fleste af os efterhånden, og der er noget om snakken. Billeder er visuelle udtryk, der kan give bedre forståelse for dit indhold på hjemmesiden, samtidig med at det gør teksten letfordøjelig. Med denne opgave skal I tage stilling til...","categories": ["Overgangsaktiviter"],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/main/bliv-klar-til-folkebibliotekernes-cms/7billedmateriale/",
+        "teaser": null
+      },{
+        "title": "Opgave 8: Taksonomi - kategorier og tags",
+        "excerpt":"I Folkebibliotekernes CMS har tags og kategorier som i DDB CMS. Både kategorier og tags kan bruges på tværs af indholdstyper. Tags og kategorier anvendes til filtrering af indhold i lister og komponenter. For eksempel kan en slider (nyheds- eller artikelkarrusel) på en sektionsside sættes op til at kun at...","categories": ["Overgangsaktiviter"],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/main/bliv-klar-til-folkebibliotekernes-cms/8taksonomi/",
+        "teaser": null
+      },{
+        "title": "Opgave 9: Overblik over integrationer",
+        "excerpt":"Integrationerne på jeres hjemmeside har stor betydning for de muligheder og tilbud I tilbyder jeres brugere. Derfor er det vigtigt at se på inden overgangen til den nye hjemmeside, så I tager højde for disse funktioner – også på det nye site. Mål Målet med denne opgave er at danne...","categories": ["Overgangsaktiviter"],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/main/bliv-klar-til-folkebibliotekernes-cms/9integrationer/",
         "teaser": null
       },{
         "title": "Bliv klar til Folkebibliotekernes CMS",
         "excerpt":"Her kan I danne jer et overblik over de aktiviteter, I skal igennem ifm. overgangen til Folkebibliotekernes CMS. Tag stilling til indholdet på jeres nuværende website og skitsér en plan for det nye. Kig på de nye designelementer og lad jer inspirere til en ny måde at strukturere og vise...","categories": [],
         "tags": [],
         "url": "/folkebibliotekernes_cms_manual/main/bliv-klar-til-folkebibliotekernes-cms/",
-        "teaser": null
-      },{
-        "title": "Opgave 3: Identitetsfarve",
-        "excerpt":"Bibliotekets egen unikke identitetsfarve bruges på en lang række elementer i brugergrænsefladen. I har måske haft flere farver på jeres nuværende hjemmeside, men nu skal I kun vælge en. Metode og fremgangsmåde I skal vælge en identitetsfarve for jeres hjemmeside. I skal vælge en stærk og ren farve, da den...","categories": ["Overgangsaktiviter"],
-        "tags": [],
-        "url": "/folkebibliotekernes_cms_manual/main/bliv-klar-til-folkebibliotekernes-cms/identitetsfarve/",
-        "teaser": null
-      },{
-        "title": "Opgave 4: Informationsarkitektur og navigation",
-        "excerpt":"Designbureauet Dept har lavet grunddesign til Folkebibliotekernes CMS. Dept har arbejdet med et særligt navigationsprincip, som det er værd at sætte sig ind i. Jeres nye hjemmeside bliver bedst, hvis I benytter design systemet, som det er tiltænkt. Med navigation hub konceptet navigerer man i dybden i stedet for bredden....","categories": ["Overgangsaktiviter"],
-        "tags": [],
-        "url": "/folkebibliotekernes_cms_manual/main/bliv-klar-til-folkebibliotekernes-cms/informationsarkiktektur/",
-        "teaser": null
-      },{
-        "title": "Opgave 9: Overblik over integrationer",
-        "excerpt":"Integrationerne på jeres hjemmeside har stor betydning for de muligheder og tilbud I tilbyder jeres brugere. Derfor er det vigtigt at se på inden overgangen til den nye hjemmeside, så I tager højde for disse funktioner – også på det nye site. Mål Målet med denne opgave er at danne...","categories": ["Overgangsaktiviter"],
-        "tags": [],
-        "url": "/folkebibliotekernes_cms_manual/main/bliv-klar-til-folkebibliotekernes-cms/integrationer/",
-        "teaser": null
-      },{
-        "title": "Opgave 2: Tilretning af logo til nyt design ",
-        "excerpt":"Det kan godt tage tid at designe et nyt logo, så tænk allerede nu over, om bibliotekets logo overholder de krav, der er til det nye CMS. Måske får det betydning for hele bibliotekets visuelle identitet, så det er vigtigt hurtigst muligt at få det på plads. Metode og fremgangsmåde...","categories": ["Overgangsaktiviter"],
-        "tags": [],
-        "url": "/folkebibliotekernes_cms_manual/main/bliv-klar-til-folkebibliotekernes-cms/logo/",
-        "teaser": null
-      },{
-        "title": "Opgave 5: Udvælg indhold der skal flyttes med over",
-        "excerpt":"En flytning kan være en god lejlighed til at få ryddet op i gammelt indhold. I denne opgave får I nogle tips til at få kvalificeret det indhold, som skal med over på den nye hjemmeside. Der er ingen minimumskrav til hvor mange sider, artikler, licenser osv. der skal flyttes...","categories": ["Overgangsaktiviter"],
-        "tags": [],
-        "url": "/folkebibliotekernes_cms_manual/main/bliv-klar-til-folkebibliotekernes-cms/oprydning/",
-        "teaser": null
-      },{
-        "title": "Opgave 1: Projektorganisering på biblioteket",
-        "excerpt":"Målet med denne opgave er at få etableret en fornuftig organisering af arbejdet med at implementere Folkebibliotekernes CMS. Beskrivelse I skal nu til at opbygge en helt ny hjemmeside, og det kommer til at tage noget tid. Derfor er det vigtigt, at I får styr på, hvem der skal gøre...","categories": ["Overgangsaktiviter"],
-        "tags": [],
-        "url": "/folkebibliotekernes_cms_manual/main/bliv-klar-til-folkebibliotekernes-cms/projektorganisering-paa-biblioteket/",
-        "teaser": null
-      },{
-        "title": "Opgave 8: Taksonomi - kategorier og tags",
-        "excerpt":"I Folkebibliotekernes CMS har tags og kategorier samme struktur og funktion som i DDB CMS. Tags kan bruges på tværs af indholdstyper, mens kategorier er bundet til en bestemt indholdstype som et arrangement eller en artikel. Tags og kategorier anvendes til filtrering af indhold i lister og komponenter. For eksempel...","categories": ["Overgangsaktiviter"],
-        "tags": [],
-        "url": "/folkebibliotekernes_cms_manual/main/bliv-klar-til-folkebibliotekernes-cms/taksonomi/",
         "teaser": null
       },{
         "title": "Biblioteksindstillinger",
@@ -281,6 +287,12 @@ var store = [{
         "url": "/folkebibliotekernes_cms_manual/main/emneord/reserveringer/",
         "teaser": null
       },{
+        "title": "Søgning",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/main/emneord/s%C3%B8gning/",
+        "teaser": null
+      },{
         "title": "URL omdirigeringer",
         "excerpt":"Redirects  ","categories": [],
         "tags": [],
@@ -312,7 +324,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Dashboard",
-        "excerpt":"[INDSÆT SKÆRMBILLEDE AF DASHBOARD] Dashboardet er en samlet indgang for brugeren til alt, hvad der vedrører brugerens lån, reserveringer, gebyrer etc. Brugeren kommer til Dashboardet ved at klikke på Dit overblik i brugermenuen. Indholdet på siden tilpasser sig brugeren, så denne kun får vist relevant indhold – fx Reserveringer klar...","categories": ["Lånerstatus og brugerprofil"],
+        "excerpt":"[ INDSÆT SKÆRMBILLEDE AF DASHBOARD ] Dashboardet er en samlet indgang for brugeren til alt, hvad der vedrører brugerens lån, reserveringer, gebyrer etc. Brugeren kommer til Dashboardet ved at klikke på Dit overblik i brugermenuen. Indholdet på siden tilpasser sig brugeren, så denne kun får vist relevant indhold – fx...","categories": ["Lånerstatus og brugerprofil"],
         "tags": [],
         "url": "/folkebibliotekernes_cms_manual/main/nye-features/dashboard/",
         "teaser": null
