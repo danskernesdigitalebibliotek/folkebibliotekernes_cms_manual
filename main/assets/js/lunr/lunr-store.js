03f7307447f70c4d1ed99f68c219d0c98e79262b
JBechAalborg <JBechAalborg@users.noreply.github.com>
2024-02-21 09:02:08 +0000

    Create/update Jekyll site for main 50957f23202e3f83973c405fb98ffff88d3f9a1f

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -378,7 +378,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Opret systembruger og skift password",
-        "excerpt":"I topmenuen klik på Personer Eller åbn via URL (udskift mit-domænenavn.dk): https://mit-domænenavn.dk/admin/people Opret systembruger Klik på Personer i topmenuen. Klik på knappen Personer Klik på knappen Add user i øverste højre hjørne. Klik på knappen Add user Du skal nu udfylde en formular med oplysninger om den nye systembruger. Se...","categories": ["Konfiguration"],
+        "excerpt":"I topmenuen klik på Personer Eller åbn via URL (udskift mit-domænenavn.dk): https://mit-domænenavn.dk/admin/people Opret systembruger Klik på knappen Add user i øverste højre hjørne. Klik på knappen Add user Du skal nu udfylde en formular med oplysninger om den nye systembruger. Se et eksempel på formularen nedenfor. Indtast oplysningerne i formularen:...","categories": ["Konfiguration"],
         "tags": [],
         "url": "/folkebibliotekernes_cms_manual/main/konfiguration/personer/",
         "teaser": null
