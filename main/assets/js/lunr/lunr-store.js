638bfab34167fb8a2534837ea45aa80d0824fd31
agnetemoos <agnetemoos@users.noreply.github.com>
2024-02-27 14:28:58 +0000

    Create/update Jekyll site for main e20a1d72fa716d9d30a25ba1935a40f737bdb39c

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -330,7 +330,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Artikel",
-        "excerpt":"Artikel svarer til den indholdstype, der i det gamle CMS hed ”Nyhed”. Sådan opretter du en artikel I topmenuen klik på Indhold eller åbn https://mit-domænenavn.dk/admin/content (udskift mit- domænenavn.dk) Klik på Tilføj indhold i øverste højre hjørne: Klik på Article: Artiklens indhold opbygges via et hovedindholdsfelt (findes der er bedre ord??) til venstre og et sidebar-panel...","categories": ["Indholdstyper"],
+        "excerpt":"Artikel svarer til den indholdstype, der i det gamle CMS hed ”Nyhed”. Sådan opretter du en artikel I topmenuen klik på Indhold eller åbn https://mit-domænenavn.dk/admin/content (udskift mit- domænenavn.dk) Klik på Tilføj indhold i øverste højre hjørne: Tilføj indhold Klik på Article: Klik på Article Artiklens indhold opbygges via et hovedindholdsfelt til venstre og et sidebar-panel...","categories": ["Indholdstyper"],
         "tags": [],
         "url": "/folkebibliotekernes_cms_manual/main/indhold/artikel/",
         "teaser": null
