a5f19b85f61c60285527971bd01eb502b79b4fdc
JBechAalborg <JBechAalborg@users.noreply.github.com>
2024-03-05 11:56:08 +0000

    Create/update Jekyll site for main a1e4f577f5865efbeb444deb10016f596f0597af

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -372,7 +372,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Rediger hovedmenuen",
-        "excerpt":"I topmenuen klik på Struktur - &gt; Menuer eller https://mit-domænenavn.dk/admin/structure/menu (udskift mit-domænenavn.dk) Klik på Redigér menu i linjen Main navigation. Klik på knappen Tilføj link i øverste højre hjørne. Udfyld formularen med følgende oplysninger: Titel: Ordet, der vises i topmenuen Link: Linkadresse til menupunktet Aktiveret: skal menupunktet være synligt eller...","categories": ["Menuer"],
+        "excerpt":"I topmenuen klik på Struktur - &gt; Menuer eller https://mit-domænenavn.dk/admin/structure/menu (udskift mit-domænenavn.dk) Vælg Struktur og herefter Menuer i topmeuen Klik på Redigér menu i linjen Main navigation. Klik på knappen Redigér menu Klik på knappen Tilføj link i øverste højre hjørne. Udfyld formularen med følgende oplysninger: Titel: Ordet, der vises...","categories": ["Menuer"],
         "tags": [],
         "url": "/folkebibliotekernes_cms_manual/main/indhold/hovedmenu/",
         "teaser": null
