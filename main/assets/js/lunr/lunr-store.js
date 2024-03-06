f04d1b8af0faa1ca2b86236057b72c61b39a69d8
agnete-allmail <agnete-allmail@users.noreply.github.com>
2024-03-06 10:28:15 +0000

    Create/update Jekyll site for main 36776c2dbf48dc895075b26e34a7d5623f7dca78

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -383,6 +383,12 @@ var store = [{
         "url": "/folkebibliotekernes_cms_manual/main/indhold/kampagner/",
         "teaser": null
       },{
+        "title": "Testside",
+        "excerpt":"          Her er et kort over Nydamskolen        ","categories": ["Indholdstyper"],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/main/indhold/test/",
+        "teaser": null
+      },{
         "title": "FBS indstillinger",
         "excerpt":"I topmenuen klik på Indstillinger &gt; Web-services &gt; FBS indstillinger   Eller åbn via URL (udskift mit-domænenavn.dk):  https://mit-domænenavn.dk/admin/config/services/fbs   Om FBS indstillinger  Opsæt forbindelse til bibliotekssystemet.   Anbefalede indstillinger for FBS                  Feltnavn       Værdi                       FBS Service URL       https://fbs-openplatform.dbc.dk                     FBS Service URL        ","categories": ["Konfiguration"],
         "tags": [],
--- main/indhold/test.md
+++
@@ -1,3 +0,0 @@
-
-
-{% include figure class="sixty" image_path="https://github.com/danskernesdigitalebibliotek/folkebibliotekernes_cms_manual/assets/162302286/6c7d809d-b705-40d0-a8b7-0d35e805680b" alt="Her er et kort over Nydamskolen" caption="Her er et kort over Nydamskolen" %} 
