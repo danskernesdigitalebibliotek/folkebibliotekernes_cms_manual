0b5ca87aca804df72d8e62f9c7df57b0fee11b78
agnetemoos <agnetemoos@users.noreply.github.com>
2024-03-13 15:41:38 +0000

    Create/update Jekyll site for main d302191cf634b4c719d95f266d82cbe1c9f8c89c

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -444,7 +444,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Velkommen",
-        "excerpt":"Velkomstside (node/12) - Sættes som “Redirect on create” - /admin/config/people/registration.                  Se eksempel på: Velkommen       DPL CMS (reload.dk)           Denne side redirectes brugere til ved succesfuld brugeroprettelse via hjemmesiden.   ","categories": ["Faste sider"],
+        "excerpt":"Velkomstside (node/12) - Sættes som “Redirect on create” - /admin/config/people/registration.                  Se eksempel på: Velkommen       DPL CMS (reload.dk)           Denne side redirectes brugere til ved succesfuld brugeroprettelse via hjemmesiden.   ","categories": [],
         "tags": [],
         "url": "/folkebibliotekernes_cms_manual/main/indhold/velkommen/",
         "teaser": null
