e1e4f46a5061cfdea63a2dd9d8e477b061ddd65d
agnetemoos <agnetemoos@users.noreply.github.com>
2024-03-13 15:41:59 +0000

    Create/update Jekyll site for main d0edbd7ca3d1f1b8bea6398710ddfc06cf6a4699

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -432,7 +432,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Takster",
-        "excerpt":"Takster (node/9) - Sættes som “Fees and replacement cost URL” - /admin/config/dpl-library-agency/fees Se eksempel på: Takster | DPL CMS (reload.dk) Der linkes til denne side flere steder, bla. Fra Gebyrer &amp; Erstatninger, men også fra materialer som er ved at udløbe.   ","categories": ["Faste sider"],
+        "excerpt":"Takster (node/9) - Sættes som “Fees and replacement cost URL” - /admin/config/dpl-library-agency/fees Se eksempel på: Takster | DPL CMS (reload.dk) Der linkes til denne side flere steder, bla. Fra Gebyrer &amp; Erstatninger, men også fra materialer som er ved at udløbe.   ","categories": [],
         "tags": [],
         "url": "/folkebibliotekernes_cms_manual/main/indhold/takster/",
         "teaser": null
