2fb3ec6f119d5f45878c20ab6a86bc4faabb6d00
agnetemoos <agnetemoos@users.noreply.github.com>
2024-03-13 15:33:34 +0000

    Create/update Jekyll site for main a343510e0c01b083292c5d0678aa07babc5fcedc

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -401,12 +401,6 @@ var store = [{
         "url": "/folkebibliotekernes_cms_manual/main/indhold/om-faste-sider/",
         "teaser": null
       },{
-        "title": "Pausefunktion",
-        "excerpt":"Pausefunktion er en informationside om at sætte sine reserveringer på pause. Det er en standardside eller “fast side”. På brugernes reserveringsoversigt, er der et link til denne side. INDSÆT BILLEDE Tilret teksten for Pausefunktion INDSÆT BILLEDE Standardteksten må gerne tilrettes: Find siden der hedder Pausefuktion. Den hedder også node/8. Tryk...","categories": ["Faste sider"],
-        "tags": [],
-        "url": "/folkebibliotekernes_cms_manual/main/indhold/pausefunktion/",
-        "teaser": null
-      },{
         "title": "Privatlivspolitik",
         "excerpt":"Privatlivspolitik (node/10) - Skal linkes til fra servicemenuen i footeren under “Behandling af persondata”. Denne er på nuværende tidspunkt hardcoded, hvilket rettes i kommende release. Det ønskes at titlen på linket skal være “Privatlivspolitik” så det matcher “Privatlivspolitik” siden. Der linkes til denne side under “Delete patron link” på /admin/config/dpl-library-agency/patron-page-settings...","categories": ["Faste sider"],
         "tags": [],
@@ -417,6 +411,12 @@ var store = [{
         "excerpt":"Reglement (node/11) - Der linkes på nuværende tidspunkt ikke til denne side nogen steder. Det er oplagt at linke til denne side fra brugeroprettelses siden. Det ville også give mening at linke til reglementet fra servicemenuen. Menupunktet “Servicedeklaration” kan erstattes med “Reglement”. Se eksempel på Reglement DPL CMS (reload.dk) Det...","categories": ["Faste sider"],
         "tags": [],
         "url": "/folkebibliotekernes_cms_manual/main/indhold/reglement/",
+        "teaser": null
+      },{
+        "title": "Reserveringspause",
+        "excerpt":"Pausefunktion er en informationside om at sætte sine reserveringer på pause. Det er en standardside eller “fast side”. På brugernes reserveringsoversigt, er der et link til denne side. INDSÆT BILLEDE Tilret teksten for Pausefunktion INDSÆT BILLEDE Standardteksten må gerne tilrettes: Find siden der hedder Pausefuktion. Den hedder også node/8. Tryk...","categories": ["Faste sider"],
+        "tags": [],
+        "url": "/folkebibliotekernes_cms_manual/main/indhold/reserveringspause/",
         "teaser": null
       },{
         "title": "Opret side",
