ce2ea46989f0b5d4bfc7d4db9ec03cae29d2e63c
agnetemoos <agnetemoos@users.noreply.github.com>
2024-03-15 11:15:04 +0000

    Create/update Jekyll site for main 7ee180184892993b8692adb927bef9f02ec9b35e

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -450,7 +450,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Reserveringspause",
-        "excerpt":"Pausefunktion er en informationside om at sætte sine reserveringer på pause. Det er en standardside eller “fast side”. På brugernes reserveringsoversigt, er der et link til denne side. INDSÆT BILLEDE Tilret teksten for Pausefunktion INDSÆT BILLEDE Standardteksten må gerne tilrettes: Find siden der hedder Pausefuktion. Den hedder også node/8. Tryk...","categories": ["Generelt"],
+        "excerpt":"Pausefunktion er en informationside om at sætte sine reserveringer på pause. Det er en standardside eller “fast side”. På brugernes reserveringsoversigt, er der et link til denne side. INDSÆT BILLEDE Tilret teksten for Pausefunktion INDSÆT BILLEDE Standardteksten må gerne tilrettes: Find siden der hedder Pausefuktion. Den hedder også node/8. Tryk...","categories": [],
         "tags": [],
         "url": "/folkebibliotekernes_cms_manual/main/indhold/reserveringspause/",
         "teaser": null
