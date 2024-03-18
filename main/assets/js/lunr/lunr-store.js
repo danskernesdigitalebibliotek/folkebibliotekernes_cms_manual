c8e26385356a4e0cbb0835ced0a60611b2ecdb3d
anmapls <anmapls@users.noreply.github.com>
2024-03-18 14:36:09 +0000

    Create/update Jekyll site for main 4618146689671e55c4e7e65d193bd3812f3675a0

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -438,7 +438,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Paragraphs Komponenterne",
-        "excerpt":"Beskrivelse af Paragraphs-elementernes indhold/funktioner Accordion En type indhold med en liste af overskrifter stablet oven på hinanden. Når der klikkes på en overskrift, vil den enten åbne eller lukke en tilknyttet indholdsrude. Card grid – automatic Den automatiske variant af Card grid-komponenten kan automatisk trække spots for indhold ind på...","categories": ["Generelt"],
+        "excerpt":"De komponenter, som du opbygger dit indhold i artikler, begivenheder og sider af, kaldes Paragraphs. Beskrivelse af Paragraphs-komponenternes indhold og funktioner Accordion En type indhold med en liste af overskrifter stablet oven på hinanden. Når der klikkes på en overskrift, vil den enten åbne eller lukke en tilknyttet indholdsrude. Card...","categories": ["Generelt"],
         "tags": [],
         "url": "/folkebibliotekernes_cms_manual/main/indhold/paragraphs-komponenter/",
         "teaser": null
