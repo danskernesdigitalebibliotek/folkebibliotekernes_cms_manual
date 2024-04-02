5df91ab20662ce8bfdb6159b76adff5f905d57f0
agnetemoos <agnetemoos@users.noreply.github.com>
2024-04-02 13:28:42 +0000

    Create/update Jekyll site for main 3e5dd2a98558c1e737da15e760235ea88cbb88bf

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -630,7 +630,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Release Notes 2024.11.0",
-        "excerpt":"Nye features Artikelliste: /articles lister oprettede artikler. Der er mulighed for at filtrere på artikelkategorier. Redigeringslås på alle indholdstyper: Forhindrer at flere redaktører redigerer i det samme indhold samtidig. Alle redaktører har mulighed at lukke en lås op, i tilfælde at låsen er aktiv uden grund. Automatisk materialeparagraph: Tilføj en...","categories": ["Release Notes"],
+        "excerpt":"Nye features Artikelliste: Under /articles lister oprettede artikler. Der er mulighed for at filtrere på artikelkategorier. Redigeringslås på alle indholdstyper: Forhindrer at flere redaktører redigerer i det samme indhold samtidig. Alle redaktører har mulighed at lukke en lås op, i tilfælde at låsen er aktiv uden grund. Automatisk materialeparagraph: Tilføj...","categories": ["Release Notes"],
         "tags": [],
         "url": "/folkebibliotekernes_cms_manual/main/release-notes/release-notes-2024-11-0/",
         "teaser": null
