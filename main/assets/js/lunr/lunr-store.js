def40eeb612f24230d56895209d1c1888cbd9af3
agnetemoos <agnetemoos@users.noreply.github.com>
2024-04-09 09:05:12 +0000

    Create/update Jekyll site for main ed093c2145ebbcd062f8bb322f463756362be1f5

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -504,7 +504,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Indstillinger for gebyrside",
-        "excerpt":"I topmenuen klik på Indstillinger &gt; Biblioteksindstillinger &gt; Indstillinger for gebyrside Eller åbn via URL (udskift mit-domænenavn.dk): https://mit-domænenavn.dk/admin/config/dpl-library-agency/fees Blokeret bruger - Link i modal til blokeret bruger (til fx Mit betalingsoverblik) Hvis en bruger er blokeret pga. af overskredet gebyrgrænse vises en pop-up til brugeren ved login. I den pop-up...","categories": ["Konfiguration"],
+        "excerpt":"I topmenuen klik på Indstillinger &gt; Biblioteksindstillinger &gt; Indstillinger for gebyrside Eller åbn via URL (udskift mit-domænenavn.dk): https://mit-domænenavn.dk/admin/config/dpl-library-agency/fees Blokeret bruger - Link til blokeret bruger som vises i modal Blokeret bruger - Link i modal til blokeret bruger (til fx Mit betalingsoverblik) Hvis en bruger er blokeret pga. af overskredet...","categories": ["Konfiguration"],
         "tags": [],
         "url": "/folkebibliotekernes_cms_manual/main/konfiguration/gebyrindstillinger/",
         "teaser": null
