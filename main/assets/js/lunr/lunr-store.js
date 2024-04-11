6231831d3dfc3469f51f7a69203ba5a28ae8ee74
agnetemoos <agnetemoos@users.noreply.github.com>
2024-04-11 08:55:21 +0000

    Create/update Jekyll site for main 7647f1136e1c9cd7ee47be3b288a4c0a769c0469

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -425,12 +425,6 @@ var store = [{
         "url": "/folkebibliotekernes_cms_manual/main/indhold/listeside-artikler/",
         "teaser": null
       },{
-        "title": "Biblioteker",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "/folkebibliotekernes_cms_manual/main/indhold/listeside-biblioteker/",
-        "teaser": null
-      },{
         "title": "Paragraphs Komponenterne",
         "excerpt":"De komponenter, som du opbygger dit indhold i artikler, begivenheder og sider af, kaldes Paragraphs. Klik på + Add for at tilføje de paragraphs, som du ønsker, at din side skal bestå af. Klik på + Add Paragraphs kan tilføjes både over og under allerede indsatte paragraphs. Beskrivelse af Paragraphs-komponenternes...","categories": ["Generelt"],
         "tags": [],
