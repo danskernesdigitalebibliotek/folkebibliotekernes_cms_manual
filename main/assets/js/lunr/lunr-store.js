1b012380a9c5f9abcdf827e91b321b84dd203831
agnetemoos <agnetemoos@users.noreply.github.com>
2024-04-11 09:00:53 +0000

    Create/update Jekyll site for main bcd228683f758eefbabe40995023d2c396e5bcc0

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -402,7 +402,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Opret indhold i Folkebibliotekernes CMS",
-        "excerpt":"Redaktionelt indhold er alt det indhold, som I selv opretter på jeres site. Sider Artikler Arrangementer Åbningstider Biblioteker Kampagner Hovedmenu og footermenu Tags og kategorier Faste sider Folkebibliotekernes CMS fødes med nogle faste sider eller standardsider. Faste sider er præcis som alle andre sider. De er der bare fra starten...","categories": [],
+        "excerpt":"Redaktionelt indhold er alt det indhold, som I selv opretter på jeres site. Sider Artikler Arrangementer Åbningstider Biblioteker Kampagner Hovedmenu og footermenu Tags og kategorier Brødkrummer Faste sider Folkebibliotekernes CMS fødes med nogle faste sider eller standardsider. Faste sider er præcis som alle andre sider. De er der bare fra...","categories": [],
         "tags": [],
         "url": "/folkebibliotekernes_cms_manual/main/indhold/",
         "teaser": null
