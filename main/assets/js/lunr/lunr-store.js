fd26f6b571a2f1faf8f100e8cbe9d14046a4d805
agnetemoos <agnetemoos@users.noreply.github.com>
2024-04-19 11:18:06 +0000

    Create/update Jekyll site for main b9cad0ff0c122ce7cf012405d1429806c468f036

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -665,7 +665,7 @@ var store = [{
         "url": "/main/release-notes/release-notes-220324/",
         "teaser": null
       },{
-        "title": "Opgave 4: Faste sider og forsiden",
+        "title": "Opgave 5: Faste sider og forsiden",
         "excerpt":"Gennemgå de faste sider Folkebibliotekernes CMS fødes med nogle faste sider eller standardsider. De faste sider kan redigeres, præcis som sider I selv har oprettet. De er der bare fra starten af, når I får systemet. Rundt omkring på jeres website, er der links til de faste sider. Hvis de...","categories": ["Startopsætning"],
         "tags": [],
         "url": "/main/startopsaetning/faste-sider-og-forside/",
