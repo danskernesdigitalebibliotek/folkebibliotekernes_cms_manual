cb13c2202aac8d4a6e58042ae15a87a69293725e
anmapls <anmapls@users.noreply.github.com>
2024-04-23 08:09:49 +0000

    Create/update Jekyll site for main 664de8dbf0debf7e21add00e478ceb5de3b259bc

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -462,7 +462,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Side",
-        "excerpt":"Indholdstypen Side bruges til at oprette forsider og lignende samlende indgange til tilknyttet indhold på dit site. FB CMS benytter betegnelserne sektionsside og navigationsside, men det henviser blot til deres funktioner – der er er ikke forskel på, hvordan man som udgangspunkt opretter dem. Du kan se eksempler på, hvordan...","categories": ["Indholdstyper"],
+        "excerpt":"Indholdstypen Side bruges til at oprette forsider og lignende samlende indgange til tilknyttet indhold på dit site. FB CMS benytter betegnelserne sektionsside og navigationsside, men det henviser blot til deres funktioner – der er ikke forskel på, hvordan man som udgangspunkt opretter dem. Du kan se eksempler på, hvordan sektions-...","categories": ["Indholdstyper"],
         "tags": [],
         "url": "/main/indhold/side/",
         "teaser": null
