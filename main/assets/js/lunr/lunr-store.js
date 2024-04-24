0053ebfe6f2f0d66ba16e33784a689b475b9e879
StefanSdv <StefanSdv@users.noreply.github.com>
2024-04-24 12:10:21 +0000

    Create/update Jekyll site for main 27cffe1dac03e93895c0b8c1fc544754b8fb42fc

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -384,7 +384,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Brødkrummesti",
-        "excerpt":"1. Planlæg strukturen af hjemmesiden For at oprette strukturen på jeres hjemmesiden, kan I benytte det arbejde, som I lavede i forberedelsesopgave 4: Informationsarkitektur og navigation. Hvis I ikke har lavet det endnu, kan det bruges som inspiration til hvordan I vil bygge siden op. Eksempel på en struktur Vi...","categories": ["Navigation"],
+        "excerpt":"Forklar hvad brødkrummer er. 1. Planlæg strukturen af hjemmesiden For at oprette strukturen på jeres hjemmesiden, kan I benytte det arbejde, som I lavede i forberedelsesopgave 4: Informationsarkitektur og navigation. Hvis I ikke har lavet det endnu, kan det bruges som inspiration til hvordan I vil bygge siden op. Eksempel...","categories": ["Navigation"],
         "tags": [],
         "url": "/main/indhold/broedkrummesti/",
         "teaser": null
