27ab04c46bca99f4b88b457e813f55ec272c9920
agnetemoos <agnetemoos@users.noreply.github.com>
2024-04-25 12:04:41 +0000

    Create/update Jekyll site for main 623a42478885c176722363aba8deda6960423407

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -690,7 +690,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Startopsætning",
-        "excerpt":"Denne guide hjælper jer med startopsætning af jeres site. Opgaverne skal udføres i den rigtige rækkefølge og man må ikke springe over.   Startopsætning - kom i gang  I kan gå i gang med startopsætningen, når I har modtaget URL og login til jeres Folkebibliotekernes CMS staging-site   ","categories": [],
+        "excerpt":"Denne guide hjælper jer med startopsætning af jeres site. Det er en god idé at gennemføre startopsætningen, inden I begynder at oprette indhold.   Startopsætning - kom i gang  I kan gå i gang med startopsætningen, når I har modtaget URL og login til jeres Folkebibliotekernes CMS staging-site   ","categories": [],
         "tags": [],
         "url": "/main/startopsaetning/",
         "teaser": null
