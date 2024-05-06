7fc96b46ffcedab17c7f4ad3a0b9c74ea2e49714
agnetemoos <agnetemoos@users.noreply.github.com>
2024-05-06 11:14:29 +0000

    Create/update Jekyll site for main 4d881b07e5bb513da3d86dd88d4f1d965390fd0a

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -564,7 +564,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Inspiration / litteratur",
-        "excerpt":"Der er et utal af muligheder for at formidle litteratur på hjemmesiden. Her er et eksempel fra Billund, hvor der er bygget et inspirationsunivers op ved hjælp af følgende paragraphs: Material grid automatic Links Der er også muligt at oprette forskellige artikler som kan trækkes ind i for eksempel et...","categories": ["Formidlingsinspiration"],
+        "excerpt":"Der er et utal af muligheder for at formidle litteratur på hjemmesiden. Her er et eksempel fra Billund, hvor der er bygget et inspirationsunivers op ved hjælp af følgende paragraphs: Material grid automatic Links Eksempel på bibliotek Der er også muligt at oprette forskellige artikler som kan trækkes ind i...","categories": ["Formidlingsinspiration"],
         "tags": [],
         "url": "/main/inspiration/inspiration-litteratur/",
         "teaser": null
