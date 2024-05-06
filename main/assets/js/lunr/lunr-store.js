63f143a7ed7149a9a70d7680589d0a11e9fe9786
agnetemoos <agnetemoos@users.noreply.github.com>
2024-05-06 08:35:15 +0000

    Create/update Jekyll site for main 013b8b63405178b50c8eaf96bde59de57fd3d864

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -521,6 +521,12 @@ var store = [{
         "url": "/main/indhold/webforms/",
         "teaser": null
       },{
+        "title": "Inspiration",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/main/inspiration/",
+        "teaser": null
+      },{
         "title": "FBS indstillinger",
         "excerpt":"I topmenuen klik på Indstillinger &gt; Web-services &gt; FBS indstillinger   Eller åbn via URL (udskift mit-domænenavn.dk):  https://mit-domænenavn.dk/admin/config/services/fbs   Om FBS indstillinger  Opsæt forbindelse til bibliotekssystemet.   Anbefalede indstillinger for FBS                  Feltnavn       Værdi                       FBS Service URL       https://fbs-openplatform.dbc.dk                  FBS Service URL        ","categories": ["Konfiguration"],
         "tags": [],
