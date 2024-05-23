25cf981c52d7e321868056c64cb3070a0b3a4910
StefanSdv <StefanSdv@users.noreply.github.com>
2024-05-23 12:03:08 +0000

    Create/update Jekyll site for main 4c8df4d913805b57928713b7b8b3861fb0dc1407

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -467,7 +467,7 @@ var store = [{
         "url": "/main/indhold/cql-soegestrenge/",
         "teaser": null
       },{
-        "title": "Logoer til DBCs digitale tilbud",
+        "title": "Logoer til DBC's digitale tilbud",
         "excerpt":"DBC har lavet en PR-pakke til deres digitale digitale tilbud. Den indeholder logoer i forskellige udgaver og i høj opløsning. Der er logo til Bibliotek.dk, Børnebiffen, Buggi, Faktalink, Filmstriben, Forfatterweb samt Læsekompasset.   Download PR-pakke med logo-filer til DBC’s tjenester.  ","categories": ["Generelt"],
         "tags": [],
         "url": "/main/indhold/dbc-logoer/",
