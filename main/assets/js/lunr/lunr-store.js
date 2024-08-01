fa0be2448baf205e26d24a5eadedd9531ca83c8c
a30j <a30j@users.noreply.github.com>
2024-08-01 08:40:51 +0000

    Create/update Jekyll site for main 0139dd426b31977b3876d5574aea5e7e51eb4640

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -821,6 +821,12 @@ var store = [{
         "url": "/main/overblik/release-notes/release-notes-2024-27-1/",
         "teaser": null
       },{
+        "title": "Release Notes 2024.28.0",
+        "excerpt":"Denne release indeholder forskellige fejlrettelser og udvikling. Releasedato: 11-07-2024 Fejlrettelser Fjernlån fra Det Kongelige bibliotek og andre fagbiblioteker vises nu i lånerstatus – dog uden materialetype og forside. Mulighed for at tilføje “På hylden”, “Opstilling” og “Delopstilling” ved avanceret søgning under rediger CQL. Kontaktformular: Bekræftelsesmail slår nu igennem. Webformularer/kontaktformularer sender...","categories": ["Release Notes"],
+        "tags": [],
+        "url": "/main/overblik/release-notes/release-notes-2024-28-0/",
+        "teaser": null
+      },{
         "title": "Release Notes 220324",
         "excerpt":"Nye features Artikelliste: Under /articles lister oprettede artikler. Der er mulighed for at filtrere på artikelkategorier. Redigeringslås på alle indholdstyper: Forhindrer at flere redaktører redigerer i det samme indhold samtidig. Alle redaktører har mulighed at lukke en lås op, i tilfælde at låsen er aktiv uden grund. Automatisk materialeparagraph: Tilføj...","categories": ["Release Notes"],
         "tags": [],
