06f1627ff49d1d395f110f04d52e34e1338d4242
a30j <a30j@users.noreply.github.com>
2024-08-01 08:47:46 +0000

    Create/update Jekyll site for main d53beefe37bba2170526e6e3f7122582efc87044

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -822,7 +822,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Release Notes 2024.28.0",
-        "excerpt":"Denne release indeholder forskellige fejlrettelser og udvikling. Releasedato: 11-07-2024 Fejlrettelser Fjernlån fra Det Kongelige bibliotek og andre fagbiblioteker vises nu i lånerstatus – dog uden materialetype og forside. Mulighed for at tilføje “På hylden”, “Opstilling” og “Delopstilling” ved avanceret søgning under rediger CQL. Kontaktformular: Bekræftelsesmail slår nu igennem. Webformularer/kontaktformularer sender...","categories": ["Release Notes"],
+        "excerpt":"Denne release indeholder forskellige fejlrettelser og udvikling. Releasedato: 11-07-2024 Fejlrettelser Fjernlån fra Det Kongelige bibliotek og andre fagbiblioteker vises nu i lånerstatus – dog uden materialetype. Mulighed for at tilføje “På hylden”, “Opstilling” og “Delopstilling” ved avanceret søgning under rediger CQL. Kontaktformular: Bekræftelsesmail slår nu igennem. Webformularer/kontaktformularer sender nu også...","categories": ["Release Notes"],
         "tags": [],
         "url": "/main/overblik/release-notes/release-notes-2024-28-0/",
         "teaser": null
