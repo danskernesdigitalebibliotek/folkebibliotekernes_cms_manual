fcfe48d56358053095b65f692d19509b97026608
nino-kff <nino-kff@users.noreply.github.com>
2024-08-29 07:31:16 +0000

    Create/update Jekyll site for main 2e329a7fdfa5879106dc97a4227ae3e83261eb04

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -846,7 +846,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Release Notes 2024.35.0",
-        "excerpt":"Knap til fornyelse af flere lån fungerer igen, oplysninger i API om arrangement er relevant for billetleverandør og fejl rettet med forsvundne billeder m.m. Releasedato: 29-08-2024 Nye features Mobilvisning: Åbningstidsikon vises i stedet for huskelisteikon. Brødkrummer og sider: Hvis man ønsker at slette en side tilknyttet en brødkrumme, skal man...","categories": ["Release Notes"],
+        "excerpt":"Knap til fornyelse af flere lån fungerer igen, oplysninger i API om arrangement er relevant for billetleverandør og fejl rettet med forsvundne billeder m.m. Releasedato: 29-08-2024 Nye features Mobilvisning: Åbningstidsikon vises i stedet for huskelisteikon. Brødkrummer og sider: Hvis man ønsker at slette en side tilknyttet en brødkrumme, skal brødkrummen...","categories": ["Release Notes"],
         "tags": [],
         "url": "/main/overblik/release-notes/release-notes-2024-35-0/",
         "teaser": null
