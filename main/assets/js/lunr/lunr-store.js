f0a123f3ba57b4a87fb530952640afcc7d09bdcc
nino-kff <nino-kff@users.noreply.github.com>
2024-10-03 07:55:38 +0000

    Create/update Jekyll site for main 78d15a58fb024156c82f09535b98691664dde339

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -888,7 +888,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Release Notes 2024.40.1",
-        "excerpt":"Nye features Releasedato: 03-10-2024 Opgraderingen rulles ud til: Redaktørbiblioteker: Produktionssites opgraderes. Webmasterbiblioteker: Kun testsite opgraderes. Nye features Tags på redaktionelt indhold linker til søgeside, som lister indhold opmærket med samme tag. Bemærk at der pt. ikke vises tags på indholdstypen side endnu. Nu muligt at linke til avanceret søgning hvor...","categories": ["Release Notes"],
+        "excerpt":"Nye features Releasedato: 03-10-2024 Opgraderingen rulles ud til: Redaktørbiblioteker: Produktionssites opgraderes. Webmasterbiblioteker: Kun testsite opgraderes. Nye features Tags på redaktionelt indhold linker til søgeside, som lister indhold opmærket med samme tag. Bemærk at der pt. ikke vises tags på indholdstypen “side” endnu. Nu muligt at linke til avanceret søgning hvor...","categories": ["Release Notes"],
         "tags": [],
         "url": "/main/overblik/release-notes/release-notes-2024-40-1/",
         "teaser": null
