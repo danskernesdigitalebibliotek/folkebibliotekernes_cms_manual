86dd39f0bf951dd1d87822cc4bd29cd7ed4db19f
agnetemoos <agnetemoos@users.noreply.github.com>
2025-02-19 11:48:02 +0000

    Create/update Jekyll site for main 6ae23b9d4fdeb643b85472822c28f59b04cd3711

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -1007,6 +1007,12 @@ var store = [{
         "url": "/main/overblik/release-notes/release-notes-2025-07-1/",
         "teaser": null
       },{
+        "title": "Release Notes 2025.08.0",
+        "excerpt":"Releasedato: 20-02-2025 Redaktørbiblioteker: Produktionssites opgraderes til 2025.08.0 Webmasterbiblioteker: Produktionssites opgraderes til 2025.07.1. Modultestsites opgraderes til 2025.08.0. Nye features Der er ingen nye features. Fokus er lige nu på at indbygge delingsplatformen og børnesitet i CMS, men de nye features er endnu ikke synlige. Fejlrettelser Artikelliste: ”Vis flere”-knappen i bunden af...","categories": ["Release Notes"],
+        "tags": [],
+        "url": "/main/overblik/release-notes/release-notes-2025-08-0/",
+        "teaser": null
+      },{
         "title": "Release Notes 220324",
         "excerpt":"Nye features Artikelliste: Under /articles lister oprettede artikler. Der er mulighed for at filtrere på artikelkategorier. Redigeringslås på alle indholdstyper: Forhindrer at flere redaktører redigerer i det samme indhold samtidig. Alle redaktører har mulighed at lukke en lås op, i tilfælde at låsen er aktiv uden grund. Automatisk materialeparagraph: Tilføj...","categories": ["Release Notes"],
         "tags": [],
