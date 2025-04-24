b8048abfe153159e1893eeadf6354dbccaa59eab
Agerbak <Agerbak@users.noreply.github.com>
2025-04-24 22:10:31 +0000

    Create/update Jekyll site for main 3923c49f3125d23ed40c50303d248e622ed3d8ba

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -203,6 +203,12 @@ var store = [{
         "url": "/main/delingstjenesten/",
         "teaser": null
       },{
+        "title": "Sådan henter du indhold fra Delingstjenesten",
+        "excerpt":"Sådan henter du indhold fra Delingstjenesten Det Digitale Folkebibliotek er klar med næste skridt i lanceringen af Delingstjenesten. Herfra kan du hente indhold fra Bibliotekernes Nationale Redaktion – klar til brug på dit eget bibliotekssite. Nedenfor guider vi dig trin-for-trin. 1. Log ind via bibliotekets CMS Gå ind på dit...","categories": [],
+        "tags": [],
+        "url": "/main/delingstjenesten/import/",
+        "teaser": null
+      },{
         "title": "Åbningstider",
         "excerpt":"Åbningstider  ","categories": [],
         "tags": [],
