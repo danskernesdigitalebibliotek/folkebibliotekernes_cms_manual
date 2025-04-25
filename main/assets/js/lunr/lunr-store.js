357c39d90b463b4060334eb59e7379a5a8b87c23
Agerbak <Agerbak@users.noreply.github.com>
2025-04-25 06:50:23 +0000

    Create/update Jekyll site for main 03faf8f7e3067ee3a66e120d44dbebabf20ef7e6

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -521,12 +521,6 @@ var store = [{
         "url": "/main/indhold/cql-soegestrenge/",
         "teaser": null
       },{
-        "title": "Delingstjenesten",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "/main/overblik/delingstjenesten",
-        "teaser": null
-      },{
         "title": "Faste sider - overblik",
         "excerpt":"Folkebibliotekernes CMS fødes med nogle faste side eller standardsider. Faste sider er præcis som alle andre sider. De er der bare fra starten af, når I får systemet. Rundt omkring på jeres website, er der links til de faste sider. Hvis de mangler, risikerer I, at jeres brugere møder døde...","categories": ["Faste sider"],
         "tags": [],
