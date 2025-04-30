ee0c413496b070672399b7d2a24ced6a18dc4ac8
agnetemoos <agnetemoos@users.noreply.github.com>
2025-04-30 13:50:43 +0000

    Create/update Jekyll site for main 86d4389226acb569209b16d0d85fb3f6112b1aac

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -1091,6 +1091,12 @@ var store = [{
         "url": "/main/overblik/release-notes/release-notes-2025-17-1/",
         "teaser": null
       },{
+        "title": "Release Notes 2025.18.0",
+        "excerpt":"Releasedato: 01-05-2025 Redaktørbiblioteker: Produktionssites opgraderes til 2025.18.0 Webmasterbiblioteker: Produktionssites opgraderes til 2025.17.1. Modultestsites opgraderes til 2025.18.0. Fejlrettelser Delingstjenesten: Ved import af artikel med billed paragraph overføres billedkreditering (byline) nu korrekt. Samtidig navngives billedfilen korrekt i forhold til angivet filnavn. Arrangementsfeed til Biblioteket App fejler ikke mere, hvis det indeholder et...","categories": ["Release Notes"],
+        "tags": [],
+        "url": "/main/overblik/release-notes/release-notes-2025-18-0/",
+        "teaser": null
+      },{
         "title": "Release Notes 220324",
         "excerpt":"Nye features Artikelliste: Under /articles lister oprettede artikler. Der er mulighed for at filtrere på artikelkategorier. Redigeringslås på alle indholdstyper: Forhindrer at flere redaktører redigerer i det samme indhold samtidig. Alle redaktører har mulighed at lukke en lås op, i tilfælde at låsen er aktiv uden grund. Automatisk materialeparagraph: Tilføj...","categories": ["Release Notes"],
         "tags": [],
