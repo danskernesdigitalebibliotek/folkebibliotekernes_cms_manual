8ca3329fda19f66f257287904a3025a02776da66
nannaboxill <nannaboxill@users.noreply.github.com>
2025-06-11 10:52:42 +0000

    Create/update Jekyll site for main 7e6be6e7dafcc1b2c875585d82a180df726bd821

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -474,7 +474,7 @@ var store = [{
         "teaser": null
       },{
         "title": "GO",
-        "excerpt":"Denne side samler de seneste vejledninger og informationer om det nye GO. GO går fra at være et nationalt site til at få hver sin hjemmeside tilknyttet til Folkebibliotekernes eksisterende hjemmesider. Vejledninger GOs Formidlingskomponenter: Sådan opretter du nyt indhold på GO Materialer til bibliotekerne i forbindelse med lanceringen af GO:...","categories": [],
+        "excerpt":"Denne side samler de seneste vejledninger og informationer om det nye GO. GO går nu fra at være et nationalt site til at få hver sin hjemmeside tilknyttet til Folkebibliotekernes eksisterende hjemmesider. Vejledninger GOs Formidlingskomponenter: Sådan opretter du nyt indhold på GO Materialer til bibliotekerne i forbindelse med lanceringen af...","categories": [],
         "tags": [],
         "url": "/main/go/",
         "teaser": null
