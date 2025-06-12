8fec88789f25cfc213939e906e4aab3f442f6c15
nino-kff <nino-kff@users.noreply.github.com>
2025-06-12 07:25:44 +0000

    Create/update Jekyll site for main 67b11f7b002e0f244218875b2a91d1a7ffdc2c82

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -1175,6 +1175,12 @@ var store = [{
         "url": "/main/overblik/release-notes/release-notes-2025-23-0/",
         "teaser": null
       },{
+        "title": "Release Notes 2025.24.1",
+        "excerpt":"Udvikling til børnesitet og delingstjenesten   Releasedato: 12-06-2025   Redaktør: Produktionssites: 2025.24.1   Webmaster: Produktionssites: 2025.23.0 Moduletestsites: 2025.24.1   Nye features  Releasen indeholder ingen synlige forbedringer eller fejlrettelser til Folkebibliotekernes CMS. Der er udviklet på løsninger til børnesitet og delingstjenesten.  ","categories": ["Release Notes"],
+        "tags": [],
+        "url": "/main/overblik/release-notes/release-notes-2025-24-1/",
+        "teaser": null
+      },{
         "title": "Release Notes 220324",
         "excerpt":"Nye features Artikelliste: Under /articles lister oprettede artikler. Der er mulighed for at filtrere på artikelkategorier. Redigeringslås på alle indholdstyper: Forhindrer at flere redaktører redigerer i det samme indhold samtidig. Alle redaktører har mulighed at lukke en lås op, i tilfælde at låsen er aktiv uden grund. Automatisk materialeparagraph: Tilføj...","categories": ["Release Notes"],
         "tags": [],
