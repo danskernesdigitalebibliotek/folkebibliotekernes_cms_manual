62b7f98719462d7295dd5cad4c3b528580bc38b4
chh-ddf <chh-ddf@users.noreply.github.com>
2025-07-02 12:15:39 +0000

    Create/update Jekyll site for main 1e4eb81232e6bd0dd6719dad7e918546377683d1

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -1236,7 +1236,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Release Notes 2025.27.0",
-        "excerpt":"Firstaccessiondate-søgekode bliver til firstaccessiondateitem-filter Releasedato: 02-07-2025 Redaktør: Produktionssites: 2025.27.0 Webmaster: Produktionssites: 2025.26.1 / 2025.27.0, Moduletestsites: 2025.27.0 Følgende webmasterbiblioteker overgår efter aftale til 2025.27.0 også på produktionssites: Tårnby Herning Sønderborg Ballerup Albertslund Roskilde Odense Aalborg Faxe Følgende webmasterbiblioteker har ønsket at blive deres normale rul, dvs. deres produktionsites får 2025.26.1: Aarhus...","categories": ["Release Notes"],
+        "excerpt":"Firstaccessiondate-søgekode bliver til firstaccessiondateitem-filter Releasedato: 03-07-2025 Redaktør: Produktionssites: 2025.27.0 Webmaster: Produktionssites: 2025.26.1 / 2025.27.0, Moduletestsites: 2025.27.0 Følgende webmasterbiblioteker overgår efter aftale til 2025.27.0 også på produktionssites: Tårnby Herning Sønderborg Ballerup Albertslund Roskilde Odense Aalborg Faxe Følgende webmasterbiblioteker har ønsket at blive deres normale rul, dvs. deres produktionsites får 2025.26.1: Aarhus...","categories": ["Release Notes"],
         "tags": [],
         "url": "/main/overblik/release-notes/release-notes-2025-27-0/",
         "teaser": null
