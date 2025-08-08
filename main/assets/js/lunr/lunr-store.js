d929d6da7c9b26ee01209f192381a8df7abcc840
agnetemoos <agnetemoos@users.noreply.github.com>
2025-08-08 10:28:22 +0000

    Create/update Jekyll site for main 55ee3b77cb2b423010e1083698fb750e49ff7ae6

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -1320,7 +1320,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Opgave 10: Byg sidens faste struktur og menuer",
-        "excerpt":"1. Planlæg strukturen af hjemmesiden For at oprette strukturen på jeres hjemmesiden, kan I benytte det arbejde, som I lavede i forberedelsesopgave 4: Informationsarkitektur og navigation. Hvis I ikke har lavet det endnu, kan det bruges som inspiration til hvordan I vil bygge siden op. 2. Opret jeres sider Start...","categories": ["Startopsætning"],
+        "excerpt":"1. Planlæg strukturen af hjemmesiden For at oprette strukturen på jeres hjemmesiden, kan I benytte det arbejde, som I lavede i forberedelsesopgave 4: Informationsarkitektur og navigation. Hvis I ikke har lavet det endnu, kan det bruges som inspiration til hvordan I vil bygge siden op. 2. Opret jeres sider Start...","categories": ["Basis konfiguration"],
         "tags": [],
         "url": "/main/startopsaetning/struktur-og-menuer/",
         "teaser": null
