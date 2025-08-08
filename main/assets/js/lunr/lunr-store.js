6432be468f72161182c06729390901063ff9ed28
agnetemoos <agnetemoos@users.noreply.github.com>
2025-08-08 13:42:18 +0000

    Create/update Jekyll site for main ea982149def2a975988c0725ae14c0613f15f37f

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -1338,7 +1338,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Opgave 4: Opret systembrugere",
-        "excerpt":"Du er sikkert ikke den eneste, der skal lave indhold til jeres nye hjemmeside. Derfor skal du nu oprette logins til de kolleger, der skal hjælpe dig dig med at bygge hjemmesiden. Vi anbefaler meget, at I bruger individuelle logins med kun de nødvendige rettigheder tilknyttet. I DDB CMS skulle...","categories": ["Basis konfiguration"],
+        "excerpt":"Du er sikkert ikke den eneste, der skal lave indhold til jeres nye hjemmeside. Derfor skal du nu oprette logins til de kolleger, der skal hjælpe dig dig med at bygge hjemmesiden. Vi anbefaler meget, at I bruger individuelle logins med kun de nødvendige rettigheder tilknyttet. OBS! Når du opretter...","categories": ["Basis konfiguration"],
         "tags": [],
         "url": "/main/startopsaetning/systembrugere/",
         "teaser": null
