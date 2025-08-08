fddcea73d6f13770317de954f0c4b4b9aee8a718
agnetemoos <agnetemoos@users.noreply.github.com>
2025-08-08 10:27:36 +0000

    Create/update Jekyll site for main b9358bd496821ccedf211032e6ad1d3ff99a0a76

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -1296,7 +1296,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Opgave 1: OpenID Connect (Adgangsplatformen)",
-        "excerpt":"Adgangsplatformen er meget vigtig for Folkebibliotekernes CMS. Både søgning og brugerlogin forudsætter at bibliotekets adgangsnøgler til Adgangsplatformen er indsat korrekt i backend. Adgangsplatformen bygger på en teknologi der hedder OpenID Connect. Derfor omtales de ofte synonymt. I denne opgave skal I kopiere jeres OpenID Connect adgangsnøgler fra DDB CMS over...","categories": ["Startopsætning"],
+        "excerpt":"Adgangsplatformen er meget vigtig for Folkebibliotekernes CMS. Både søgning og brugerlogin forudsætter at bibliotekets adgangsnøgler til Adgangsplatformen er indsat korrekt i backend. Adgangsplatformen bygger på en teknologi der hedder OpenID Connect. Derfor omtales de ofte synonymt. I denne opgave skal I kopiere jeres OpenID Connect adgangsnøgler fra DDB CMS over...","categories": ["Basis konfiguration"],
         "tags": [],
         "url": "/main/startopsaetning/openid-connect/",
         "teaser": null
