938553f702ca7b279c4d5d0041d0af3eac3dad0d
agnetemoos <agnetemoos@users.noreply.github.com>
2025-08-09 09:57:01 +0000

    Create/update Jekyll site for main 5b73e9fb5a2de35347763c94dae51588d1d11bc5

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -1260,7 +1260,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Basiskonfiguration - introduktion",
-        "excerpt":" ","categories": ["Basis konfiguration"],
+        "excerpt":"Staging-sitet som biblioteket modtager er uden indhold og henter ikke lånerdata og søgeposter for jeres kommune. Der er ikke etableret forbindelse til Adgangsplatformen, Søgning osv. Det første der skal ske, er at I skal lave en basiskonfiguration af jeres systemet. Det gør I ved at udføre alle basiskonfigurationsopgaver i den...","categories": ["Basis konfiguration"],
         "tags": [],
         "url": "/main/startopsaetning/basiskonfiguration-intro/",
         "teaser": null
