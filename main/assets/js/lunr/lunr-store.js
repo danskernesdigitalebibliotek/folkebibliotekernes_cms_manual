ee09a235265013eb16baa9b1cdc92a084b6e234d
agnetemoos <agnetemoos@users.noreply.github.com>
2025-08-12 10:48:15 +0000

    Create/update Jekyll site for main ca06596ca2b1ceccb2fd04b1e648976f0a385de1

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -1296,7 +1296,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Introduktion til Go-live",
-        "excerpt":" ","categories": ["Go-live"],
+        "excerpt":"Go-live datoen er den dag, hvor jeres nye website bliver synligt på internettet, og hvor jeres brugere får adgang til siden.   Den URL på som jeres nuværende hjemmeside ligger på vil på den dag begynde at pege på jeres nye Folkebibliotekernes CMS-hjemmeside.   Go-live opgaverne er beskrevet herunder.   ","categories": ["Go-live"],
         "tags": [],
         "url": "/main/startopsaetning/go-live-intro/",
         "teaser": null
