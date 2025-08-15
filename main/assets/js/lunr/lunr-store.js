091309a5661eb16738fd9d30cd9b74e3d614450a
agnetemoos <agnetemoos@users.noreply.github.com>
2025-08-15 08:16:34 +0000

    Create/update Jekyll site for main 3bc9db8dd9eb099209d8cb859f8993d66210a84a

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -168,7 +168,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Sådan abonnerer du på indhold fra Delingstjenesten",
-        "excerpt":"Med abonnement kan du automatisk og fleksibelt få det nyeste indhold leveret direkte på dit biblioteketssite, efterhånden som det publiceres. Du kan abonnere på indhold fra Delingstjenesten baseret på tags og kategorier. Når nye artikler med det valgte tag eller kategori udgives, bliver de automatisk hentet ind på dit bibliotekssite...","categories": [],
+        "excerpt":"Med abonnement kan du automatisk og fleksibelt få det nyeste indhold leveret direkte på dit biblioteketssite, efterhånden som det publiceres. Du kan abonnere på indhold fra Delingstjenesten baseret på tags og kategorier. Når nye artikler med det valgte tag eller kategori udgives, bliver de automatisk hentet ind på dit bibliotekssite...","categories": ["Delingstjenesten"],
         "tags": [],
         "url": "/main/delingstjenesten/abonnementsordning/",
         "teaser": null
