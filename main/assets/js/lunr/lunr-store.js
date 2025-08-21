7668af999816d743c0e1a6ba3492140df0ae765f
nino-kff <nino-kff@users.noreply.github.com>
2025-08-21 08:09:16 +0000

    Create/update Jekyll site for main 5213d30296d1ce56f8a6c2d722b30b161d74070e

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -882,7 +882,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Release Notes 2025.34.0",
-        "excerpt":"Nyt til arrangementer og diverse forbedringer Releasedato: 21-08-2025 Redaktør: Produktionssites: 2025.34.0 Webmaster: Produktionssites: 2025.33.0, Moduletestsites: 2025.34.0 Nye features Arrangementer: Nu muligt at oprettet arrangementer uden klokkeslæt. Det kan f.eks. være relevant i forbindelse med udstilling, som strækker sig over længere tid. Arrangementer: Nu muligt at fravælge at arrangementet vises i...","categories": ["Release Notes"],
+        "excerpt":"Nyt til arrangementer og diverse forbedringer Releasedato: 21-08-2025 Redaktør: Produktionssites: 2025.34.0 Webmaster: Produktionssites: 2025.33.0, Moduletestsites: 2025.34.0 Nye features Arrangementer: Nu muligt at oprettet arrangementer uden klokkeslæt. Det kan f.eks. være relevant i forbindelse med udstilling, som strækker sig over længere tid. Arrangementer: Nu muligt at fravælge at arrangementer vises i...","categories": ["Release Notes"],
         "tags": [],
         "url": "/main/overblik/release-notes/release-notes-2025-34-0/",
         "teaser": null
