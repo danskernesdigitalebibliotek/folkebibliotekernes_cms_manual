86981e319150eba0ea6ee777c525659faa811fb3
nino-kff <nino-kff@users.noreply.github.com>
2025-09-18 08:03:57 +0000

    Create/update Jekyll site for main 183dc981f57c9126a4b916db1878e58dcec7ef96

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -894,7 +894,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Release Notes 2025.38.0",
-        "excerpt":"Diverse forbedringer og fejlrettelser Releasedato: 18-09-2025 Redaktør: Produktionssites: 2025.38.0 Webmaster: Produktionssites: 2025.36.0, Moduletestsites: 2025.38.0 Forbedringer og fejlrettelser Fejl rettet hvor valgte filtre nulstilles på artikel- og arrangementsoversigten hvis man vælger knappen ”Vis flere”. Flere systemtekniske opdateringer f.eks. opgradering til Drupal 10.5. GO: Fejl rettet hvor lånte e-bøger listet under brugerprofilen...","categories": ["Release Notes"],
+        "excerpt":"Diverse forbedringer og fejlrettelser Releasedato: 18-09-2025 Redaktør: Produktionssites: 2025.38.0 Webmaster: Produktionssites: 2025.36.0, Moduletestsites: 2025.38.0 Forbedringer og fejlrettelser Fejl rettet hvor valgte filtre nulstilles på artikel- og arrangementsoversigten hvis man vælger knappen ”Vis flere”. Flere systemtekniske opdateringer f.eks. opgradering til Drupal 10.5 og PHP 8.3. GO: Fejl rettet hvor lånte e-bøger...","categories": ["Release Notes"],
         "tags": [],
         "url": "/main/overblik/release-notes/release-notes-2025-38-0/",
         "teaser": null
