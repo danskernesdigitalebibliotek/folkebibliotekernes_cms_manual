238d2ea3339e57c2513d4ddaa5ca739d5418f129
agnetemoos <agnetemoos@users.noreply.github.com>
2025-09-22 12:54:15 +0000

    Create/update Jekyll site for main 9680cb76ffe40d5cd411faaf6778a4962f605c57

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -1158,7 +1158,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Webmasterplanen",
-        "excerpt":"Om vejledningen Vejledningen er målrettet webmasterbiblioteker og beskriver hvordan typiske arbejdsgange for et webmasterbibliotek håndteres herunder. Hvordan du håndterer installation af moduler på testsitet og produktionssitet Hvordan du håndterer opgraderinger af moduler, der i forvejen er installeret på sitet Derudover beskrives basale driftsforhold omkring de forskellige miljøer f.eks. opgraderingscyklus for...","categories": [],
+        "excerpt":"Vejledningen er målrettet webmasterbiblioteker og beskriver hvordan typiske arbejdsgange for et webmasterbibliotek håndteres herunder. Muligheder for lokal tilpasning Hvordan du håndterer installation af moduler på testsitet og produktionssitet Hvordan du håndterer opgraderinger af moduler, der i forvejen er installeret på sitet Derudover beskrives basale driftsforhold omkring de forskellige miljøer f.eks....","categories": [],
         "tags": [],
         "url": "/main/webmasterplanen/",
         "teaser": null
