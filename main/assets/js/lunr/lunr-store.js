30408f2d490ffba32cf40e5254afea07a5987cb6
nino-kff <nino-kff@users.noreply.github.com>
2025-10-01 12:43:15 +0000

    Create/update Jekyll site for main 1e345da7b6a4e9d4fab22eaa1d25b4d9f5a77274

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -900,7 +900,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Release Notes 2025.40.0",
-        "excerpt":"Fejl ved afspilning af lydbøger rettet   Releasedato: 01-10-2025   Redaktør: Produktionssites: 2025.40.0   Webmaster: Produktionssites: 2025.38.1, Moduletestsites: 2025.40.0   Fejlrettelser      Lydbøger: Fejl rettet hvor der opstod periodisk fejl ved afspilning af lydbøger med fejlbeskeden “Der opstod en uventet fejl, prøv igen senere eller kontakt biblioteket”.  ","categories": ["Release Notes"],
+        "excerpt":"Fejl ved afspilning af lydbøger rettet   Releasedato: 02-10-2025   Redaktør: Produktionssites: 2025.40.0   Webmaster: Produktionssites: 2025.38.1, Moduletestsites: 2025.40.0   Fejlrettelser      Lydbøger: Fejl rettet hvor der opstod periodisk fejl ved afspilning af lydbøger med fejlbeskeden “Der opstod en uventet fejl, prøv igen senere eller kontakt biblioteket”.  ","categories": ["Release Notes"],
         "tags": [],
         "url": "/main/overblik/release-notes/release-notes-2025-40-0/",
         "teaser": null
