9485637a56e560e02a57c83bb033ab0758193a9e
Agerbak <Agerbak@users.noreply.github.com>
2025-10-21 20:32:22 +0000

    Create/update Jekyll site for main 5a049d819cbeaacc82088a01f33624b809814aa3

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -168,7 +168,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Fejl i abonnementslogik – sådan rydder du op",
-        "excerpt":"Fejl i abonnementslogikken er nu rettet Delingstjenestens abonnementsfunktion har i en periode opført sig uforudsigeligt. Det er nu rettet, og den forbedrede logik er allerede i drift på alle bibliotekssites. Det betyder, at du fremover kun vil modtage det indhold fra Delingstjenesten, som er korrekt tagget med det abonnement, du...","categories": [],
+        "excerpt":"Delingstjenestens abonnementsfunktion har i en periode opført sig uforudsigeligt. Det er nu rettet, og den forbedrede logik er allerede i drift på alle bibliotekssites. Det betyder, at du fremover kun vil modtage det indhold fra Delingstjenesten, som er korrekt tagget med det abonnement, du har tilmeldt. Men: Fejlen har efterladt...","categories": [],
         "tags": [],
         "url": "/main/delingstjenesten/abonnementlogik/",
         "teaser": null
