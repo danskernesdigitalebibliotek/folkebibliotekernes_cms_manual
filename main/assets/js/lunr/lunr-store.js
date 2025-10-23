70dea9df98d69790f3948c668aaa2837e997b36f
Agerbak <Agerbak@users.noreply.github.com>
2025-10-23 10:35:01 +0000

    Create/update Jekyll site for main 53f68547e2dda5708b668a553f0bc65699997b95

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -186,7 +186,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Delingstjenesten",
-        "excerpt":"Nu er det blevet endnu nemmere at holde sin hjemmeside opdateret med nyt indhold. Ud over at sende og hente indhold manuelt, kan man med den nye abonnement-funktion automatisk få det nyeste indhold leveret direkte på bibliotekets hjemmeside, efterhånden som det publiceres. Overblik - Det kan man med Delingstjenesten Besøg...","categories": [],
+        "excerpt":"Nu er det blevet endnu nemmere at holde sin hjemmeside opdateret med nyt indhold. Ud over at sende og hente indhold manuelt, kan man med abonnement-funktionen automatisk få det nyeste indhold leveret direkte på bibliotekets hjemmeside, efterhånden som det publiceres. Overblik - Det kan man med Delingstjenesten Besøg Delingstjenestens hjemmeside:...","categories": [],
         "tags": [],
         "url": "/main/delingstjenesten/",
         "teaser": null
