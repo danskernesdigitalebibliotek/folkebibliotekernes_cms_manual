aba286f1ad5bb102c196b4237442f6f7b6f9ff4d
RebekkaDDF <RebekkaDDF@users.noreply.github.com>
2026-01-09 12:53:09 +0000

    Create/update Jekyll site for main df3208df613346960a68c03c5380854994f7b2b8

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -948,7 +948,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Release Notes 2026.02.1",
-        "excerpt":"Releasedato: 08-01-2026 Redaktør: Produktionssites: 2026.02.1 Webmaster: Produktionssites: 2025.50.1, Moduletestsites: 2026.02.1 Ny udvikling Søgning fravælger materialer fra blacklistede filialer i stedet for at tilvælge materialer fra ikke-blacklistede filialer. Visning af reserveringer klar til afhentning (“Klar til dig” i Dit overblik + “Klar til lån” under Alle reserveringer) sorteres i nummerrækkefølge efter...","categories": ["Release Notes"],
+        "excerpt":"Releasedato: 09-01-2026 Redaktør: Produktionssites: 2026.02.3 Webmaster: Produktionssites: 2025.50.1, Moduletestsites: 2026.02.3 Hotfix 2026.02.3 Løser fejlen i heroen på filialvisningssiden, hvor kontaktoplysninger manglede. Releasedato: 09-01-2026 Redaktør: Produktionssites: 2026.02.2 Webmaster: Produktionssites: 2025.50.1, Moduletestsites: 2026.02.2 Hotfix 2026.02.2 Retter kode der bliver kørt når det bliver rullet ud som sørger for at nedarvede felter er...","categories": ["Release Notes"],
         "tags": [],
         "url": "/main/overblik/release-notes/release-notes-2026-02-1/",
         "teaser": null
