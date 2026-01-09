9218bc54fab0d1eb84e387fb989da8f84713e7b5
RebekkaDDF <RebekkaDDF@users.noreply.github.com>
2026-01-09 12:57:36 +0000

    Create/update Jekyll site for main ab18ba00e644e8fd943f36b52dc08db5511f0283

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -947,7 +947,7 @@ var store = [{
         "url": "/main/overblik/release-notes/release-notes-2025-50-1/",
         "teaser": null
       },{
-        "title": "Release Notes 2026.02.1",
+        "title": "Release Notes 2026.02.3",
         "excerpt":"Releasedato: 09-01-2026 Redaktør: Produktionssites: 2026.02.3 Webmaster: Produktionssites: 2025.50.1, Moduletestsites: 2026.02.3 Hotfix 2026.02.3 Løser fejlen i heroen på filialvisningssiden, hvor kontaktoplysninger manglede. Releasedato: 09-01-2026 Redaktør: Produktionssites: 2026.02.2 Webmaster: Produktionssites: 2025.50.1, Moduletestsites: 2026.02.2 Hotfix 2026.02.2 Retter kode der bliver kørt når det bliver rullet ud som sørger for at nedarvede felter er...","categories": ["Release Notes"],
         "tags": [],
         "url": "/main/overblik/release-notes/release-notes-2026-02-3/",
