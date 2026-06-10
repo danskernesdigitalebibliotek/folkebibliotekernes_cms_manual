7e6f0082f5d19d99d3f7d054f2129d08a86c20df
RebekkaDDF <RebekkaDDF@users.noreply.github.com>
2026-06-10 12:40:29 +0000

    Create/update Jekyll site for main 8d88b0198d7b6e32fae2ee13e2191bc3064c4a6d

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -983,10 +983,10 @@ var store = [{
         "url": "/main/overblik/release-notes/release-notes-2026-22-0/",
         "teaser": null
       },{
-        "title": "Release Notes 2026.23.0",
-        "excerpt":"Releasedato: 04-06-2026 Redaktør: Produktionssites: 2026.22.0 Webmaster: Produktionssites: 2026.18.4, Moduletestsites: 2026.23.0 Denne opgradering kommer på webmasterbibliotekernes moduletestsites i uge 23. Opgraderingen vil komme på redaktørbibliotekernes hjemmesider i uge 24 sammen med den øvrige release. Opgradering til Drupal 10.6 samt en opdatering af Lagoon images. Derudover er der tilføjet forbedret effektivitet i...","categories": ["Release Notes"],
+        "title": "Release Notes 2026.23.4",
+        "excerpt":"Fejlrettelser til Unilogin Releasedato: 10-06-2026 Redaktør: GO-Produktionssites: 2026.22.2 Webmaster: GO-Produktionssites: 2026.18.6 (København kommer på 2026.23.4) - Denne rettelse er bundet sammen med GO-sitesene og derfor opgraderes moduletestsitesene ikke. Hotfix der retter problemer med at logge ind med Unilogin på GO-sites. Releasedato: 04-06-2026 Redaktør: Produktionssites: 2026.22.0 Webmaster: Produktionssites: 2026.18.4, Moduletestsites: 2026.23.0...","categories": ["Release Notes"],
         "tags": [],
-        "url": "/main/overblik/release-notes/release-notes-2026-23-0/",
+        "url": "/main/overblik/release-notes/release-notes-2026-23-4/",
         "teaser": null
       },{
         "title": "Release Notes 2026.4.1",
