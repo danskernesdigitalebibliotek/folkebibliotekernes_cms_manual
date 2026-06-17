d6d6e5cc55b09a6968bd0fe13fd5bc1cdf484398
StefanSnd <StefanSnd@users.noreply.github.com>
2026-06-17 11:55:10 +0000

    Create/update Jekyll site for main c312d01e2882c93e0c7b0873a5685c68a64d5c4c

--- main/assets/js/lunr/lunr-store.js
+++ main/assets/js/lunr/lunr-store.js
@@ -534,7 +534,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Logo, identitetsfarve og favicon",
-        "excerpt":"I topmenuen klik på Udseende &gt; Novel &gt; Indstillinger Eller åbn via URL (udskift mit-domænenavn.dk): https://mit-domænenavn.dk/admin/appearance/settings/novel Folkebibliotekernes CMS bruger et standard tema, der hedder “Novel”. I kan ændre nogle få ting i standard-temaet: Logo Identitetsfarve Favicon For at kunne rette i jeres udseende skal I være logget ind, som en...","categories": ["Konfiguration"],
+        "excerpt":"Folkebibliotekernes CMS bruger et standard tema, der hedder “Novel”. I kan ændre nogle tre ting i temaet: logo, identitetsfarve og favicon. For at kunne rette i jeres udseende skal I være logget ind, som en lokal administrator. For at tilgå tilrettelse af temaet - klik på Udseende &gt; Novel &gt;...","categories": ["Konfiguration"],
         "tags": [],
         "url": "/main/konfiguration/udseende/",
         "teaser": null
